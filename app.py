ed29b98da46c8f1b5d0b1e62fc1ca90428d0b0af
Yash Goyal <yash.goyal@spyne.ai>
2026-06-26 16:49:39 +0530

    Fix OAuth 403: add target=_top to Google button so click navigates parent page not iframe

--- app.py
+++ app.py
@@ -675,7 +675,7 @@ def _login_page():
 
     if google_configured:
         auth_url = _google_auth_url(client_id, redirect_uri)
-        _btn_html = f"""<a href="{auth_url}" class="google-btn">
+        _btn_html = f"""<a href="{auth_url}" class="google-btn" target="_top">
             <svg width="18" height="18" viewBox="0 0 18 18" xmlns="http://www.w3.org/2000/svg">
               <path fill="#4285F4" d="M17.64 9.2c0-.637-.057-1.251-.164-1.84H9v3.481h4.844a4.14 4.14 0 0 1-1.796 2.716v2.259h2.908c1.702-1.567 2.684-3.875 2.684-6.615z"></path>
               <path fill="#34A853" d="M9 18c2.43 0 4.467-.806 5.956-2.18l-2.908-2.259c-.806.54-1.837.86-3.048.86-2.344 0-4.328-1.584-5.036-3.711H.957v2.332A8.997 8.997 0 0 0 9 18z"></path>
